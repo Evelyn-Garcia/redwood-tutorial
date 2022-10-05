6a15957ba9f6b4c324b5c82ab11903eaf2c5f3a0
Evelyn-Garcia <71730349+Evelyn-Garcia@users.noreply.github.com>
2022-10-04 22:02:46 -0400

    Chapter 6
    Multiple Comments
    Adding Comments to the Schema

---
+++ api/src/services/comments/comments.js
@@ -0,0 +1,23 @@
+import { db } from 'src/lib/db'
+
+export const comments = () => {
+  return db.comment.findMany()
+}
+
+export const comment = ({ id }) => {
+  return db.comment.findUnique({
+    where: { id },
+  })
+}
+
+export const Comment = {
+  post: (_obj, { root }) => {
+    return db.comment.findUnique({ where: { id: root?.id } }).post()
+  },
+}
+
+export const deleteComment = ({ id }) => {
+  return db.comment.delete({
+    where: { id },
+  })
+}
